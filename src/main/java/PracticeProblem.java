338938c60bad282f7ec7025cdfe01d823eb48e24
katherine-zhou-ics4u1-02-25 <katherine.zhou3@student.tdsb.on.ca>
2025-02-12 14:53:17 +0000

    Update Practice

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class PracticeProblem {
 
 	public static void main(String args[]) {
@@ -5,5 +6,24 @@ public class PracticeProblem {
 	}
 
 	//Write your functions here
+	public static int evenOrOdd(int even, int odd){
+		Scanner input = new Scanner (System.in);
+		int num = input.nextInt();
+		if (num % 2 == 0) {
+			System.out.println("even");
+		}
+		else {
+			System.out.println("Odd");
+		}
+		return num;
+
+	public static String teacherOrStudent(String word){
+		if
+
+
+	}
+
+
+	}
 
 }
